7c86be43e535b1a331b331c3d2e7a79f3797040e
Sheyman0382 <adegbiteseyi22@gmail.com>
2022-09-09 17:41:58 -0700

    Write a C program that prints exactly

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print exactly 
+ *
+ * Return: Always 0 (Moving it)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}

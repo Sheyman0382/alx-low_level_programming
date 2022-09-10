328707a91f4c4d7d8c27b1a0b230c3d7dbca7d8e
Sheyman0382 <adegbiteseyi22@gmail.com>
2022-09-09 18:44:15 -0700

    Write a script that generates the assembly code (Intel syntax)

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+
+/**
+ * main - what a better way
+ *
+ * Return: Always 1 (moved)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is usefull\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}

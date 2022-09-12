a101a21ddc8251f20d1ae011d1784cd2b944ec11
Sheyman0382 <adegbiteseyi22@gmail.com>
2022-09-12 16:43:32 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+  * main - a program that prints alphabets in lower case
+  * Description - I sometimes suffer from insomnia
+  * Return: 0 Always (successful)
+  */
+
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}

16e3979d1b1cb49c83ca539d5f53f0505b1d1f5c
Sheyman0382 <adegbiteseyi22@gmail.com>
2022-09-12 15:48:55 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+  * main - a program that prints alphabets in lower case
+  * Description - I sometimes suffer from insomnia
+  * Return: 0 Always (successful)
+  */
+
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}

14206278785be9d5be8984f760c064efafbee6ec
Sheyman0382 <adegbiteseyi22@gmail.com>
2022-09-12 15:41:25 -0700

    Alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+  * main - a program that prints alphabets in lower case
+  * Description - I sometimes suffer from insomnia
+  * Return: 0 Always (successful)
+  */
+
+int main(void)
+{
+	char c = 'a';
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (!(c == 'q' || c == 'e'))
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}

5f3796521c306b9bdcd0fc56bfa4f6e7659a5b99
Sheyman0382 <adegbiteseyi22@gmail.com>
2022-09-12 14:17:58 -0700

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main(void);
+
+/**
+  * main - a program that prints alphabets in lower case
+  * Description - I sometimes suffer from insomnia
+  * Return: 0 Always (successful)
+  */
+
+int main(void);
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar("\n");
+	return (0);
+}

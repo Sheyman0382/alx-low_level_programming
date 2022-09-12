5e55923ba4d59cab614cddbf6aebd31951ca311f
Sheyman0382 <adegbiteseyi22@gmail.com>
2022-09-12 10:24:34 -0700

    Sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,6 @@ int main(void)
 	printf("Size of an int: %li byte(s)\n", sizeof(i));
 	printf("Size of a long int: %li byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %li byte(s)\n", sizeof(lii));
-	printf("Size of a float: %li bytes(s)\n", sizeof(f));
+	printf("Size of a float: %li byte(s)\n", sizeof(f));
 	return (0);
 }

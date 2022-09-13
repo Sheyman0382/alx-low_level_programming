1a83341d7864849c7435c6d73e9486d3d65f1333
Sheyman0382 <adegbiteseyi22@gmail.com>
2022-09-12 17:47:47 -0700

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+  * main - a program that prints alphabets in lower case
+  * Description - I sometimes suffer from insomnia
+  * Return: 0 Always (successful)
+  */
+
+int main(void)
+{
+	int c, j;
+
+	for (c = 0; c <= 9; c++)
+	{
+
+		for (j = 1; j <= 9; j++)
+		{
+			if (j > c)
+			{
+				putchar(c + '0');
+			|	putchar(j + '0');
+			
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}

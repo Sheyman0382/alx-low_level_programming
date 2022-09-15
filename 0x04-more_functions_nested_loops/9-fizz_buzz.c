db3729ab50e959f751fe3d634954620463f5aa20
Sheyman0382 <adegbiteseyi22@gmail.com>
2022-09-15 15:51:38 -0700

    Task 9hard

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -24,14 +24,15 @@ int main(void)
 		}
 		else if ((num % 5) == 0)
 		{
-			if (num != 100)
-			{
-				printf("Buzz ");
-			}
-			else
-			{
-				printf("Buzz");
-			}
+			printf("Buzz");
+		}
+		if (num != 100)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("Buzz");
 		}
 		else
 		{

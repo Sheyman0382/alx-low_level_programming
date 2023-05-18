88a75e9c9f6522f4077e882c654520794db1e4a9
Sheyman0382 <adegbiteseyi22@gmail.com>
2023-05-18 10:47:13 -0700

    task 1 to 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints a string using printf function
+ *
+ * return: Always (0)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

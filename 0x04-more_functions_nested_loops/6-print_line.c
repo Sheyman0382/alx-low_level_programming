46e69439441970781672fffed663396994653136
Sheyman0382 <adegbiteseyi22@gmail.com>
2023-05-26 02:01:15 +0000

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - a function that prints the nth lines
+ *
+ * @n: n represents the number of times '_' will be printed
+ * Return: Always (0)
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+		_putchar('_');
+}

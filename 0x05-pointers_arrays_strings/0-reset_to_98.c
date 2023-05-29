5e2c4f432a2799f4a334c0a6014376a7edb3f960
Sheyman0382 <adegbiteseyi22@gmail.com>
2023-05-29 19:46:01 +0000

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that dereference an integer
+ *
+ * @n: n is a pointer variable that stores an integer
+ * Return: Always (0)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

f92773946489bd62a869a912ed59c4e302163980
Sheyman0382 <adegbiteseyi22@gmail.com>
2023-06-02 02:45:22 +0000

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - a function that encodes a string into 1337
+ *
+ * @str: a string to be converted or worked on
+ * return: Always(0)
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (str[i] == a[j])
+			{
+				str[i] = b[j];
+			}
+		}
+	}
+	return(str);
+}

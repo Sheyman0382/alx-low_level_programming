3edc387dc22ef9e070d689bb61c31d19b4de5cd1
Sheyman0382 <adegbiteseyi22@gmail.com>
2023-06-02 02:31:05 +0000

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,13 +14,17 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; sep[i] != '\0'; i++)
+		for (j = 0; sep[j] != '\0'; j++)
 		{
-			if (str[0] >= 'a' && str[0] <= 'z')
-				str[0] = str[0] - 32;
-			if (str[i] == sep[j] && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
+			if (i == 0)
 			{
-				str[i + 1] = str[i + 1] - 32;
+				if (str[0] >= 'a' && str[0] <= 'z')
+					str[0] = str[0] - 32;
+			}
+			if (str[i] == sep[j])
+			{
+				if(str[i + 1] >= 'a' && str[i + 1] <= 'z')
+					str[i + 1] = str[i + 1] - 32;
 			}
 		}
 	}

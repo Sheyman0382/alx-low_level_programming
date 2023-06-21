f066fdb6dc1fbb8fa1e7e6e081b4492ee7d0659e
Sheyman0382 <adegbiteseyi22@gmail.com>
2023-06-20 20:15:50 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,14 +4,14 @@
  * main - a program that prints the total number of command
  * line arguments entered
  *
- * @argc: command line argument count 
+ * @argc: command line argument count
  * @argv: command line argumnt varariable
  * Return: Return something
  */
 
 int main(int argc, char *argv[])
 {
-	printf("%s \n", argv);
-	printf("%d \n", argc);
+	(void)argv;
+	printf("%d\n", argc);
 	return (0);
 }

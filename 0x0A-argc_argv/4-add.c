4c436e94b0078108b83b38245a72c1f9e96cd4a5
Sheyman0382 <adegbiteseyi22@gmail.com>
2023-06-22 08:14:01 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -24,9 +24,8 @@ int main(int argc, char **argv)
 					printf("Error\n");
 					return (1);
 				}
-				else
-					sum = sum + atoi(argv[i]);
 			}
+			sum = sum + atoi(argv[i]);
 		}
 		printf("%d\n", sum);
 	}

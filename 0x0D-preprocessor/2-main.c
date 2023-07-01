91ba6bfe042a4e70a511e0b883a22d5a0a97c110
Sheyman0382 <adegbiteseyi22@gmail.com>
2023-07-01 03:38:10 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - a program that prints the name of the current file
+ * using a predefined macro
+ *
+ * Return: Nothing
+ */
+
 int main(void)
 {
 	printf("%s\n", __FILE__);

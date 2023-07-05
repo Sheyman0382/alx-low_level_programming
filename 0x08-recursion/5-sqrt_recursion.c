5fed72288362b89fdf7f5f9120dfde89149ee8a6
Sheyman0382 <adegbiteseyi22@gmail.com>
2023-07-05 16:32:48 -0700

    4-6

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,31 +1,31 @@
- #include "main.h"
-  2
-  3 /**
-  4  * sqrt2 - a function that checks if the square of a number
-  5  * is equal to the number given
-  6  *
-  7  * @a: the square root of the number to be found
-  8  * @b: the squre of the number to be checked
-  9  * Return: return something
- 10  */
- 11
- 12 int sqrt2(int a, int b)
- 13 {
- 14         if (a == b * b)
- 15                 return (b);
- 16         if (b * b > a)
- 17                 return (-1);
- 18         return (sqrt2(a, b + 1));
- 19 }
- 20 /**
- 21  * _sqrt_recursion - a function that prints the square ro0t
- 22  * of a number
- 23  *
- 24  * @n: the square root of a number to be found
- 25  * Return: an integer
- 26  */
- 27
- 28 int _sqrt_recursion(int n)
- 29 {
- 30         return (sqrt2(n, 1));
- 31 }
+#include "main.h"
+
+/**
+ * sqrt2 - a function that checks if the square of a number
+ * is equal to the number given
+ *
+ * @a: the square root of the number to be found
+ * @b: the squre of the number to be checked
+ * Return: return something
+ */
+
+int sqrt2(int a, int b)
+{
+	if (a == b * b)
+		return (b);
+	if (b * b > a)
+		return (-1);
+	return (sqrt2(a, b + 1));
+}
+/**
+ * _sqrt_recursion - a function that prints the square ro0t
+ * of a number
+ *
+ * @n: the square root of a number to be found
+ * Return: an integer
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (sqrt2(n, 1))
+}

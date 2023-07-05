e5aacbd8a6744f6bb88d6c2f9101854703e3cc8b
Sheyman0382 <adegbiteseyi22@gmail.com>
2023-07-05 16:00:37 -0700

    0-puts_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,19 +1,17 @@
- #ifndef MAIN_H
-  2 #define MAIN_H
-  3
-  4 int _putchar(char c);
-  5 void _puts_recursion(char *s);
-  6 void _print_rev_recursion(char *s);
-  7 int _strlen_recursion(char *s);
-  8 int factorial(int n);
-  9 int _pow_recursion(int x, int y);
- 10 int _sqrt_recursion(int n);
- 11 int sqrt2(int a, int b);
- 12 int is_prime_number(int n);
- 13 int isprimem(int a, int b);
- 14 int is_palindrome(char *s);
- 15 int _lenght(char *s);
- 16 int checkp(int i, int lg, char *s);
- 17
- 18
- 19 #endif
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int sqrt2(int a, int b);
+int is_prime_number(int n);
+int isprimem(int a, int b);
+int is_palindrome(char *s);
+int _lenght(char *s);
+int checkp(int i, int lg, char *s);
+
+#endif

a32e3a6daa80f2aa6559817a559855a8c79a1e32
Sheyman0382 <adegbiteseyi22@gmail.com>
2023-07-05 16:16:26 -0700

    1-3

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,23 +1,23 @@
- #include "main.h"
-  2
-  3 /**
-  4  * _strlen_recursion - a function that returns the length of
-  5  * a string recursively
-  6  *
-  7  * @s: pointer to a string parameter
-  8  * Return: Always (0)
-  9  */
- 10
- 11 int _strlen_recursion(char *s)
- 12 {
- 13         int count = 0;
- 14
- 15         if (s[0] == '\0')
- 16         {
- 17                 return (count);
- 18         }
- 19         else
- 20         {
- 21                 return (1 + (_strlen_recursion(s + 1)));
- 22         }
- 23 }
+#include "main.h"
+
+/**
+ * _strlen_recursion - a function that returns the length of
+ * a string recursively
+ *
+ * @s: pointer to a string parameter
+ * Return: Always (0)
+ */
+ 
+int _strlen_recursion(char *s)
+{
+	int count = 0;
+
+	if (s[0] == '\0')
+	{
+		return (count);
+	}
+	else
+	{
+		return (1 + (_strlen_recursion(s + 1)));
+	}
+}
--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,22 +1,21 @@
 #include "main.h"
-  2
-  3 /**
-  4  * factorial - a function that calculates the factorial of a number
-  5  *
-  6  * @n: the number's factorial to be calculated
-  7  * Return: an integer
-  8  */
-  9
- 10 int factorial(int n)
- 11 {
- 12         if (n < 0)
- 13                 return (-1);
- 14         if (n == 0)
- 15         {
- 16                 return (1);
- 17         }
- 18         else
- 19         {
- 20                 return (n * factorial(n - 1));
- 21         }
- 22 }
+/**
+ * factorial - a function that calculates the factorial of a number
+ *
+ * @n: the number's factorial to be calculated
+ * Return: an integer
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}

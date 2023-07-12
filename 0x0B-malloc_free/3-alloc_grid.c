239df00ab3c25775d50392e6f30e51ec7f1c2ba4
Sheyman0382 <adegbiteseyi22@gmail.com>
2023-07-12 03:27:33 +0000

    unique concept

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,31 +19,30 @@ int **alloc_grid(int width, int height)
 	if (width == 0 || height == 0)
 		return (NULL);
 	ptr = malloc(sizeof(int *) * height);
-	if (ptr != NULL)
+	if (ptr == NULL)
 	{
-		for (i = 0; i < height; i++)
-		{
-			ptr[i] = malloc(sizeof(int) * width);
-			if (ptr[i] != NULL)
-			{
-				for (j = 0; j < width; j++)
-				{
-					ptr[i][j] = 0;
-				}
-			}
-			else
-			{
-				while (i >= 0)
-				{
-					free(ptr[i]);
-					i--;
-				}
-				free(ptr);
-				ptr = NULL;
-			}
-		}
-		return (ptr);
-	}
-	else
+		free(ptr);
 		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = malloc(sizeof(int) * width);
+		if (ptr[i] == NULL)
+		{
+			while (i >= 0)
+			{
+				free(ptr[i]);
+				i--;
+			}
+			return (NULL);
+		}
+	}
+	for (i = 0; j < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			ptr[i][j] = 0;
+		}
+	}
+	return (ptr);
 }

b2c6d9f08a9667e72c5b574779952e596143f9d3
Sheyman0382 <adegbiteseyi22@gmail.com>
2023-07-13 21:49:14 +0000

    correction

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -42,7 +42,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		free(ptr);
 		return (NULL);
 	}
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; i < count1; i++)
 	{
 		ptr[i] = s1[i];
 	}

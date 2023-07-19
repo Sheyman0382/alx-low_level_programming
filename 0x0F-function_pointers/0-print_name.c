c97bb0f1f63075afc8117465004ae11a2913ddeb
Sheyman0382 <adegbiteseyi22@gmail.com>
2023-07-19 19:04:57 +0000

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 
 /**
  * print_name - a function that prints a name with the
@@ -12,5 +13,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	(*f)(name);
 }

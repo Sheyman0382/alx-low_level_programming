1caf677daa6ad545b657a6de2db3ba2ab4c3d930
Sheyman0382 <adegbiteseyi22@gmail.com>
2023-07-21 22:26:14 +0000

    task_1 correction

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_numbers - a function that prints numbers
+ *
+ * @separator: the separators betweeen the numbers
+ * @n: the numbers of arguments to be summed up
+ * Return: An integer
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list sheyman;
+	unsigned int i;
+	int j;
+
+	va_start(sheyman, n);
+	for (i = 0; i < n; i++)
+	{
+		j = va_arg(sheyman, int);
+		if (i != (n - 1))
+		{
+			printf("%d%s", j, separator);
+		}
+		else
+		{
+			printf("%d\n", j);
+		}
+	}
+	va_end(sheyman);
+}

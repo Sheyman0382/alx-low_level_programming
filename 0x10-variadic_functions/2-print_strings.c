739d5e0ce2de8b8f67e7ecc1b66411b3170a63d5
Sheyman0382 <adegbiteseyi22@gmail.com>
2023-07-21 23:29:50 +0000

    task_2 correction

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		j = va_arg(sheyman, char *);
 		if (j == NULL)
-			printf("nil");
+			printf("(nil)");
 		else
 			printf("%s", j);
 		if (i != (n - 1) && separator != NULL)

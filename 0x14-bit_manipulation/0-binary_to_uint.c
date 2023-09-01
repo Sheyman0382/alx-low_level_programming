11d9b4e207965e0a049bd4332a709d925e7fda4f
Sheyman0382 <adegbiteseyi22@gmail.com>
2023-09-01 03:14:50 +0000

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,64 +1,35 @@
-#include "main.h"
+#include "holberton.h"
 
-
-int _strlen(const char *str)
-{
-	int i, count = 0;
-	while (str[i])
-	{
-		count++;
-		i++;
-	}
-	return (count);
-}
-
-
-/*
- * power - a function that calculates the exponential of an integer
- * @a: the number to be exponented
- * @b: the exponential value
+/**
+ * binary_to_uint - Converts binary to unsigned int
  *
- * Return: Return an integer
- */
-
-int power(int a, int b)
-{
-	int pow = 1;
-
-	while (b != 0)
-	{
-		pow = pow * a;
-		b--;
-	}
-	return (pow);
-}
-
-/*
- * binary_to_uint - a program that converts a binary to unit
+ * @b: Pointer to string of chars
  *
- * @b: a pointer to a stream of characters
- * Return: An integer
+ * Return: Converted number
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-	char str[] = b;
-	int base, len, num, exp, result, pow = 0;
-	int sum_result = 0;
+	signed int index = 0, num = 0;
 
-	len = (_strlen(str) - 1);
-	while (len <= 0)
+
+	if (!b)
+		return (0);
+
+	while (b[index] != '\0')
 	{
-		base = 2;
-		num = str[len] - '0';
-		if (num == 1)
+		if (b[index] != '0' && b[index] != '1')
 		{
-			exp = power(base, pow);
-			result = num * exp;
-			sum_result = sum_result + result;
+			return (0);
 		}
-		pow++;
-		len--;
+
+		num <<= 1;
+
+		if (b[index] & 1)
+		{
+			num += 1;
+		}
+		index += 1;
 	}
-	return (sum_result);
+	return (num);
 }

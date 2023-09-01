e623a3fea9d2b6d695e1322e856609a6961ef2e0
Sheyman0382 <adegbiteseyi22@gmail.com>
2023-09-01 02:36:28 +0000

    lets go

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,12 +2,14 @@
 
 /**
  * get_bit - a function that gets the index value of a bianry
- * @index: the index to be determined
  * @n: the number to operate on
- * @ Return: 0 0r 1;
+ * @index: the index to be determined
+ * Return: 0 0r 1;
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if (index > 64)
+		return (-1);
 	return ((n >> index) & 1);
 }

fa36e96c80855da948cc5456d9cd0823405f371b
Sheyman0382 <adegbiteseyi22@gmail.com>
2024-09-05 11:19:45 +0000

    my pen on the papper

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,19 +9,15 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *cursor;
-	listint_t **temp = head;
+	listint_t *temp;
+	listint_t *next_node;
 
-	if (temp != NULL)
+	temp = *head;
+	while (temp != NULL)
 	{
-		while (*head != NULL)
-		{
-			cursor = *head;
-			free(cursor);
-			*head = (*head)->next;
-		}
-
-		*temp = NULL;
-
+		next_node = temp->next;
+		free(temp);
+		temp = next_node;
 	}
+	*head = NULL;
 }

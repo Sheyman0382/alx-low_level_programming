b32381ca875582f9e9f1833353525fe905f7cbc5
Sheyman0382 <adegbiteseyi22@gmail.com>
2024-09-06 18:37:29 +0000

    101-square_matrix_map.py

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses a listint_t linked list
+ * @head: Pointer to the head of the list.
+ *
+ * Return: the head pointer to the reversed linked list.
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL, *next_node, *current;
+
+	current = next_node = *head;
+	while (current != NULL)
+	{
+		next_node = current->next;
+		current->next = prev;
+		prev = current;
+		current = next_node;
+	}
+	*head = prev;
+}
